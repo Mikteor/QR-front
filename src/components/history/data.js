dcf08b10bae37e645038a5c652a805ca75b3e89b
Mikteor <dm.pustovitenko@gmail.com>
2021-04-15 15:57:49 +0300

    layout: history

---
+++ src/components/history/data.js
@@ -0,0 +1,54 @@
+export const header = {
+    name: 'Имя Фамилия',
+    phone: 'Телефон',
+    email: 'Почта',
+    rebound:'Погашение',
+    total: 'Общий выигрыш',
+}
+
+export const data = [
+    {
+        name: 'Андрей Фамилия',
+        phone: '+7978-800-80-80',
+        email: 'test@gmail.com',
+        rebound: true,
+        total: 500,
+    },
+    {
+        name: 'Андрей Фамилия',
+        phone: '+7978-800-80-80',
+        email: 'test@gmail.com',
+        rebound: true,
+        total: 5300,
+    },
+    {
+        name: 'Андрей Фамилия',
+        phone: '+7978-800-80-80',
+        email: 'test@gmail.com',
+        rebound: true,
+        total: 1500,
+    },
+    {
+        name: 'Андрей Фамилия',
+        phone: '+7978-800-80-80',
+        email: 'test@gmail.com',
+        rebound: false,
+        total: 5010,
+    },
+    {
+        name: 'Андрей Фамилия',
+        phone: '+7978-800-80-80',
+        email: 'test@gmail.com',
+        rebound: false,
+        total: 500,
+    },
+    {
+        name: 'Андрей Фамилия',
+        phone: '+7978-800-80-80',
+        email: 'test@gmail.com',
+        rebound: false,
+        total: 100,
+    },
+    
+]
+
--- src/pages/history.js
+++ src/pages/history.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import styles from '../styles/history.module.css'
+import {header, data} from '../components/history/data'
+import TableRow from '../components/history/tableRow'
+import FilterRow from '../components/history/filters'
 
 const History = () => {
 
     return(
         <div>
-            hi history
+            <h1>История</h1>
+            <div className={styles.filterContainer}>
+                    <FilterRow />
+                    <button>Импорт в XLS</button>
+                </div>
+            <table className='tableWide'>
+                <TableRow data={header} header/>
+                {data.map((el,i)=>{
+                    return(
+                        <TableRow data={el} body/>
+                    )
+                })}
+                
+            </table>
         </div>
     )
 }
