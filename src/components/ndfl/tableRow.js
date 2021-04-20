62cacfe0bda7b6ab3ad3c7badd867a74cbf59ddc
Mikteor <dm.pustovitenko@gmail.com>
2021-04-20 19:58:28 +0300

    ndfl table fix

--- src/components/ndfl/tableRow.js
+++ src/components/ndfl/tableRow.js
@@ -14,8 +14,8 @@ const TableRow = ({data, header, footer, body}) => {
             <td>{data.email}</td>
             <td>{header? data.rebound: body && data.rebound? 'Да' : 'Нет'}</td>
             <td>{data.prize_sum}{body && ' рублей'}</td> 
-            <td>{data.activation}</td> 
-            <td>{data.ndfl}</td> 
+            {/* <td>{data.activation}</td>  */}
+            <td>{data.sum_ndfl}</td> 
         </tr>
     )
 }
