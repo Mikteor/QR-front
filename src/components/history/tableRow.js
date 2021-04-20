4460579588f629b475b8b5f7d542c563723ab28f
eug-seleznev <evgeny.s@buro82.ru>
2021-04-20 17:24:43 +0300

    download

--- src/components/history/tableRow.js
+++ src/components/history/tableRow.js
@@ -9,6 +9,7 @@ const TableRow = ({data, header, footer, body}) => {
         <tr className={
             header? styles.tableHeader : 
             styles.tableBody}>
+            <td>{data.date.split('T')[0]}</td>
             <td style={{color: data.player? 'black':'grey'}}>{data.player ? data.player.fullname: data.validated? 'неизвестно':'N/P'}</td>
             <td style={{color: data.player? 'black':'grey'}}>{data.player ?  data.player.phone:  data.validated?  'неизвестно':'N/P'}</td>
             <td style={{color: data.player? 'black':'grey'}}>{data.player ?  data.player.email:  data.validated?  'неизвестно':'N/P'}</td>
