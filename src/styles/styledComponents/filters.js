7fdcaafc4d02dd84c95ea927bb63860f7761b97e
Mikteor <dm.pustovitenko@gmail.com>
2021-04-23 19:17:58 +0300

    filter marign

--- src/styles/styledComponents/filters.js
+++ src/styles/styledComponents/filters.js
@@ -4,6 +4,7 @@ export const Input = styled.input`
 
 border: none;
 width: 200px;
+margin-right:10px;
 `
 
 export const Select = styled.select`
@@ -11,5 +12,5 @@ export const Select = styled.select`
 border: none;
 width: 200px;
 color: grey;
-
+margin-right:10px;
 `
